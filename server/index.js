6517927bc513a1a422f68c42a6a0bc88a8b377e8
PrimeXBroker <it@primexbroker.com>
2024-09-10 17:46:55 +0400

    updated

--- server/index.js
+++ server/index.js
@@ -16,10 +16,25 @@ app.use(
 );
 app.use(cookieParser());
 app.use("/auth", UserRouter);
-
+const isProduction = process.env.NODE_ENV === "production";
 mongoose.connect(process.env.MONGODB_URL).then(() => {
   console.log("MONGO DB Connected Success!");
 });
+
+process.on("unhandledRejection", (err) => {
+  console.error("Unhandled Rejection:", err.message);
+  process.exit(1);
+});
+
+process.on("uncaughtException", (err) => {
+  console.error("Uncaught Exception:", err.message);
+  process.exit(1);
+});
+if (isProduction) {
+  console.log("Running in production mode");
+} else {
+  console.log("Running in development mode");
+}
 
 app.listen(process.env.PORT, () => {
   console.log(`Server Started Listening on PORT ${process.env.PORT}`);

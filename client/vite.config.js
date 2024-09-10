c9fcb461f215cc8b4916bb1e1069c9d1af06733c
PrimeXBroker <it@primexbroker.com>
2024-09-10 19:23:18 +0400

    updated vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,14 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
